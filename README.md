fdf3d27df2efdc2f0d8be6d7036bc0fa08b6701a
rfyiamcool <rfyiamcool@163.com>
2017-11-08 08:19:05 +0800

    update doc

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cgrep
+
+### Usage:
+
+```
+cat cgrep.py |./cgrep.py  "(line|self)"
+cat cgrep.py |./cgrep.py  "xiaorui\.cc"
+```
--- cgrep.py
+++ cgrep.py
@@ -8,40 +8,49 @@ from termcolor import colored, COLORS
 from itertools import starmap
 
 
-class Colorer:
+class ReplaceColor:
     def __init__(self, regex, color):
         self.regex = re.compile(regex)
         self.color = color
 
     def __call__(self, line):
-        return self.regex.sub(lambda x: colored(x.group(0), self.color[0], attrs=["reverse", "blink"]), line)
+        # return self.regex.sub(lambda x: colored(x.group(0), self.color[0], attrs=["reverse", "blink"]), line)
+        if not line:
+            return False, None
+
+        res = self.regex.search(line)
+        if not res:
+            return False, None
+
+        return True, self.regex.sub(colored(res.group(0), self.color[0], attrs=["reverse", "blink"]), line)
 
 
 def process(line):
-    for colorer in COLORERS:
-        line = colorer(line)
+    m = False
+    cline = line
+    for colorer_call in colorers:
+        status, cline = colorer_call(cline)
+        if status:
+            m = status
+
+    if is_show_all and not m:
+        return line
+
+    if is_show_all and m:
+        return cline
+
+    if not is_show_all and not m:
+        return None
+
+    if not is_show_all and m:
+        return cline
+
     return line
-
-
-def main():
-    if len(sys.argv) == 1:
-        sys.stderr.write("Usage: tail -f some.log | %s 'one.*pattern' 'another'\n" % sys.argv[0])
-        sys.exit(1)
-
-    if len(sys.argv[1:]) > len(COLORS):
-        sys.stderr.write('Num arguments should not be more than %s.\n' % len(COLORS))
-        sys.exit(1)
-
-    try:
-        for line in sys.stdin.xreadlines():
-            sys.stdout.write(process(line))
-    except (IOError, KeyboardInterrupt):
-        pass
 
 
 COLORS = sorted([c for c in COLORS.keys() if c not in ('grey', 'white')])
 COLORS = zip(COLORS, [[]] * len(COLORS)) + zip(COLORS, [['reverse']] * len(COLORS))
-COLORERS = list(starmap(Colorer, zip(sys.argv[1:], COLORS)))
+colorers = None
 is_show_all = False
 is_v = False
 regex_args = ""
@@ -57,17 +66,20 @@ def parse_cmd():
     global is_v
     global regex_args
     global is_ignore_upper
+    global colorers
 
     try:
-        opts, regex_args = getopt.getopt(sys.argv[1:], "vhi:e:", ["re"])
+        opts, regex_args = getopt.getopt(sys.argv[1:], "avhi:e:", ["re"])
+        colorers = list(starmap(ReplaceColor, zip(regex_args, COLORS)))
+
     except getopt.GetoptError:
         usage()
         sys.exit(2)
 
     for opt, arg in opts:
         if opt == '-h':
-            print 'test.py -i <inputfile> -o <outputfile>'
-            sys.exit()
+            usage()
+            sys.exit(2)
 
         elif opt == "-v":
             is_v = True
@@ -79,5 +91,24 @@ def parse_cmd():
             is_show_all = True
 
 
+def main():
+    if len(sys.argv) == 1:
+        sys.stderr.write("Usage: tail -f debug.log | %s 'one.*pattern' 'another'\n" % sys.argv[0])
+        sys.exit(1)
+
+    if len(sys.argv[1:]) > len(COLORS):
+        sys.stderr.write('Num arguments should not be more than %s.\n' % len(COLORS))
+        sys.exit(1)
+
+    try:
+        for line in sys.stdin.xreadlines():
+            cline = process(line)
+            if cline:
+                sys.stdout.write(cline)
+    except (IOError, KeyboardInterrupt):
+        pass
+
+
 if __name__ == '__main__':
+    parse_cmd()
     main()
